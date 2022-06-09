b19fe4c00cb7848d021cfd39583ce99610dd98a6
silvajpedro <belojpedro.31@gmail.com>
2022-06-08 21:26:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "joão"
+let num1 ="31"
+let anoAtual = 2022
+let quartaFeira = true
+
+ anoAtual = anoAtual - num1
+
+console.log(anoAtual)
+
+console.log("Estou mexendo no console.log")
+
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
